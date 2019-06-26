171b167385466fc4690d413ae1b2c09565f9e9a0
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 03:14:30 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-conffile',
+    version='1.0',
+    description='A minimalist config file parser',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-conffile',
+    packages=['conffile'])
