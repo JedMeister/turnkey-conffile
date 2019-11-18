cd4b0f2b6ed3b334975fa078582cf4c7c8fe7a6d
Jeremy Davis <jeremy@jeremydavis.org>
2019-11-19 00:49:25 +1100

    First iteration of conffile v2 - needs testing.

--- conffile/__init__.py
+++ conffile/__init__.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2010 Alon Swartz <alon@turnkeylinux.org>
-#               2019 TurnKey GNU/Linux <admin@turnkeylinux.org>
+# Copyright (c) 2019 TurnKey GNU/Linux <admin@turnkeylinux.org>
 #
 # turnkey-conffile is open source software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -7,25 +6,24 @@
 # License, or (at your option) any later version.
 
 import os
-
+from pathlib import Path
 
 class ConfFileError(Exception):
     pass
 
 
-class ConfFile(dict):
+class ConfFile:
     """Configuration file class (targeted at simple shell type configs)
+
+       Note v2 is not compatible with previous v1 ConfFile
 
     Usage:
 
-        class foo(ConfFile):
-            CONF_FILE = /path/to/conf
-            REQUIRED = ['arg1' ,'arg2']
+        conf = ConfFile('path/to/conf', ['req_arg1', 'req_arg2'])
 
-        conf = foo()
-        print conf.arg1     # display ARG1 value from /path/to/conf
-        conf.arg2 = value   # set ARG2 value
-        conf.write()        # write new/update config to /path/to/conf
+        print(conf.req_arg1) # display ARG1 value from /path/to/conf
+        conf.arg2 = value    # set ARG2 value
+        conf.write()         # write new/update config to /path/to/conf
 
     Format:
 
@@ -34,64 +32,79 @@ class ConfFile(dict):
         AGE=29
 
     """
-    CONF_FILE = None
-    REQUIRED = []
-    SET_ENVIRON = False
+    def __init__(self, conf_file=None, required=[], error=True):
+        self._dict = {}
+        self.file = conf_file
+        self.path = Path(conf_file)
+        self.required = required
+        self.read(error=error)
+        self.validate_required(required, error=error)
 
-    def __init__(self):
-        self.read()
-        self.validate_required()
-        if self.SET_ENVIRON:
-            self.set_environ()
-
-    def validate_required(self, required=[]):
-        """raise exception if required arguments are not set
-        REQUIRED validated by default, but can be optionally extended
+    def validate_required(self, required=[], error=True):
+        """Check if required values exist in conf. If all set, return True.
+        If not all set and error=True, then raise exception, otherwise return
+        False.
         """
-        self.REQUIRED.extend(required)
-        for attr in self.REQUIRED:
-            if attr not in self:
-                error = "%s not specified in %s" % (attr.upper(),
-                                                    self.CONF_FILE)
-                raise ConfFileError(error)
+        for attr in required:
+            if attr not in self._dict:
+                if error:
+                    raise ConfFileError(
+                        "'{attr}' not specified in {self.conf_file}.")
+                else:
+                    return False
+        return True
 
-    def set_environ(self):
-        """set environment (run on initialization if SET_ENVIRON)"""
-        for key, val in list(self.items()):
-            os.environ[key.upper()] = val
-
-    def read(self):
-        if not self.CONF_FILE or not os.path.exists(self.CONF_FILE):
+    def read(self, error=True):
+        conf_file = self.conf_path
+        if not conf_file.is_file():
+            if error:
+                raise ConfFileError(f"Conf file '{conf_file}' not found.")
             return
 
-        with open(self.CONF_FILE) as fob:
+        with conf_file.open() as fob:
             for line in fob:
                 line = line.rstrip()
                 if not line or line.startswith("#"):
                     continue
-
-                key, val = line.split("=")
-                self[key.strip().lower()] = val.strip()
+                key, *val = line.split("=")
+                self._dict[key.strip().lower()] = ''.join(val).strip()
 
     def write(self):
-        with open(self.CONF_FILE, "w") as fob:
+        with self.conf_path.open("w") as fob:
             items = list(self.items())
             items.sort()
             for key, val in items:
                 print("%s=%s" % (key.upper(), val), file=fob)
 
     def items(self):
-        items = []
-        for key in self:
-            items.append((key, self[key]))
+        return self._dict.items()
 
-        return items
+    def keys(self):
+        return self._dict.keys()
 
-    def __getattr__(self, key):
+    def values(self):
+        return self._dict.values()
+
+    def __iter__(self):
+        return self._dict.__iter__()
+
+    def __contains__(self, key):
+        return self._dict.__contains__(key)
+
+    def __len__(self):
+        return self._dict.__len__()
+
+    def __delete__(self, key);
         try:
-            return self[key]
+            self._dict.__delete__(key)
         except KeyError as e:
-            raise AttributeError(e)
+            raise KeyError(e.msg)
 
-    def __setattr__(self, key, val):
-        self[key] = val
+    def __getitem__(self, key):
+        try:
+            return self._dict__getitem__(key)
+        except KeyError as e:
+            raise KeyError(e.msg)
+
+    def __setitem__(self, key, val):
+        self._dict.__setitem__(key, value)
